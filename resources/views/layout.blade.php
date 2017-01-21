6d6fe8dd2973631b4a9c98ff9cc08990bc38b977
vasile.rus <vasileRusGit@gmail.com>
2017-01-21 23:18:23 +0200

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Project Flyer</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
